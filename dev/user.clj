adebd05e23de71384294b1f138683e8c40223059
Pedro Girardi <pedro@plando.work>
2020-02-22 00:27:43 +0700

    Datafy

--- dev/user.clj
+++ dev/user.clj
@@ -1,5 +1,8 @@
 (ns user
   (:refer-clojure :exclude [load])
+  (:require [clojure.core.protocols :as p]
+            [clojure.datafy :as datafy]
+            [clojure.tools.namespace.repl :refer [refresh]])
   (:import (javax.imageio ImageIO)
            (java.io File)
            (java.awt.image BufferedImage)
@@ -7,11 +10,11 @@
            (ai.djl.modality.cv.util BufferedImageUtils)
            (ai.djl.mxnet.zoo MxModelZoo)
            (ai.djl.training.util ProgressBar)
-           (ai.djl.modality.cv ImageVisualization)
+           (ai.djl.modality.cv ImageVisualization DetectedObjects$DetectedObject)
            (ai.djl.repository.zoo ModelLoader ZooModel)
            (ai.djl.translate Translator)
            (ai.djl.inference Predictor)
-           (ai.djl.modality Classifications$Classification)))
+           (ai.djl.modality Classifications$Classification Classifications)))
 
 (set! *warn-on-reflection* true)
 
@@ -37,7 +40,20 @@
 (defn predict [^Predictor predictor input]
   (.predict predictor input))
 
+(extend-protocol p/Datafiable
+  Classifications
+  (datafy [o]
+    #:djl.classifications{:best (datafy/datafy (.best o))
+                          :items (map datafy/datafy (.items o))})
+
+  Classifications$Classification
+  (datafy [o]
+    #:djl.classification{:class (.getClassName o)
+                         :probability (.getProbability o)}))
+
 (comment
+
+  (refresh)
 
   (def image
     (image-from-url "https://raw.githubusercontent.com/dmlc/web-data/master/gluoncv/pose/soccer.png"))
@@ -49,11 +65,7 @@
     (-> (predictor model)
         (predict image)))
 
-  (map
-    (fn [^Classifications$Classification classification]
-      {:class (.getClassName classification)
-       :probability (.getProbability classification)})
-    (.items detections))
+  (datafy/datafy detections)
 
 
   (ImageVisualization/drawBoundingBoxes image detections)
