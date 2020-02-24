ceca234bd4a5418a026179ef15d728552bf9efe5
Pedro Girardi <pedro@plando.work>
2020-02-24 09:46:53 +0700

    Core

--- dev/user.clj
+++ dev/user.clj
@@ -1,57 +1,13 @@
 (ns user
-  (:require [clojure.core.protocols :as p]
-            [clojure.datafy :as datafy]
+  (:require [clojure.datafy :as datafy]
             [clojure.tools.namespace.repl :refer [refresh]]
-            [clojure.java.io :as io])
+            [clojure.java.io :as io]
+
+            [djl-example.core :refer :all])
   (:import (javax.imageio ImageIO)
            (java.io File)
-           (java.net URL)
-           (java.awt.image BufferedImage)
 
-           (ai.djl.modality Classifications$Classification Classifications)
-           (ai.djl.modality.cv.util BufferedImageUtils)
-           (ai.djl.modality.cv ImageVisualization)
-           (ai.djl.mxnet.zoo MxModelZoo)
-           (ai.djl.training.util ProgressBar)
-           (ai.djl.repository.zoo ModelLoader ZooModel)
-           (ai.djl.translate Translator)
-           (ai.djl.inference Predictor)))
-
-(set! *warn-on-reflection* true)
-
-(extend-protocol p/Datafiable
-  Classifications
-  (datafy [o]
-    #:djl.classifications{:best (datafy/datafy (.best o))
-                          :items (map datafy/datafy (.items o))})
-
-  Classifications$Classification
-  (datafy [o]
-    #:djl.classification{:class (.getClassName o)
-                         :probability (.getProbability o)}))
-
-(defn ^BufferedImage image-from-url [^URL url]
-  (BufferedImageUtils/fromUrl url))
-
-(def available-loaders
-  #{:ssd})
-
-(defn ^ModelLoader loader [k]
-  (case k
-    :ssd MxModelZoo/SSD
-    (throw (ex-info (str "Loader " k " doesn't exist.") {:available-loaders available-loaders}))))
-
-(defn ^ZooModel load-model [k]
-  (.loadModel (loader k) (ProgressBar.)))
-
-(defn predictor
-  ([^ZooModel model]
-   (.newPredictor model))
-  ([^ZooModel model ^Translator translator]
-   (.newPredictor model translator)))
-
-(defn predict [^Predictor predictor input]
-  (.predict predictor input))
+           (ai.djl.modality.cv ImageVisualization)))
 
 (comment
 
---
+++ src/djl_example/core.clj
@@ -0,0 +1,49 @@
+(ns djl-example.core
+  (:require [clojure.core.protocols :as p]
+            [clojure.datafy :as datafy])
+  (:import (java.net URL)
+           (java.awt.image BufferedImage)
+
+           (ai.djl.modality Classifications$Classification Classifications)
+           (ai.djl.modality.cv.util BufferedImageUtils)
+           (ai.djl.mxnet.zoo MxModelZoo)
+           (ai.djl.training.util ProgressBar)
+           (ai.djl.repository.zoo ModelLoader ZooModel)
+           (ai.djl.translate Translator)
+           (ai.djl.inference Predictor)))
+
+(set! *warn-on-reflection* true)
+
+(extend-protocol p/Datafiable
+  Classifications
+  (datafy [o]
+    #:djl.classifications{:best (datafy/datafy (.best o))
+                          :items (map datafy/datafy (.items o))})
+
+  Classifications$Classification
+  (datafy [o]
+    #:djl.classification{:class (.getClassName o)
+                         :probability (.getProbability o)}))
+
+(defn ^BufferedImage image-from-url [^URL url]
+  (BufferedImageUtils/fromUrl url))
+
+(def available-loaders
+  #{:ssd})
+
+(defn ^ModelLoader loader [k]
+  (case k
+    :ssd MxModelZoo/SSD
+    (throw (ex-info (str "Loader " k " doesn't exist.") {:available-loaders available-loaders}))))
+
+(defn ^ZooModel load-model [k]
+  (.loadModel (loader k) (ProgressBar.)))
+
+(defn predictor
+  ([^ZooModel model]
+   (.newPredictor model))
+  ([^ZooModel model ^Translator translator]
+   (.newPredictor model translator)))
+
+(defn predict [^Predictor predictor input]
+  (.predict predictor input))
