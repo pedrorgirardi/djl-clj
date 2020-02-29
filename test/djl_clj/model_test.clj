4421cf1c9513f0faedabdc96e930fb433c14c63f
Pedro Girardi <pedro@plando.work>
2020-02-29 10:27:55 +0700

    Tests - Close resources

--- test/djl_clj/model_test.clj
+++ test/djl_clj/model_test.clj
@@ -6,30 +6,28 @@
 
 (deftest bert-qa-test
   (testing "Bert QA - Keyword criteria"
-    (let [input (QAInput.
-                  "When did BBC Japan start broadcasting?"
-                  "BBC Japan was a general entertainment Channel.
-                   Which operated between December 2004 and April 2006.
-                   It ceased operations after its Japanese distributor folded."
-                  384)
-
-          model (djl/load-model :bert-qa)]
-      (is (= "[december, 2004]" (-> (djl/predictor model)
-                                    (djl/predict input))))))
+    (with-open [model (djl/load-model :bert-qa)
+                predictor (djl/predictor model)]
+      (let [input (QAInput.
+                    "When did BBC Japan start broadcasting?"
+                    "BBC Japan was a general entertainment Channel.
+                     Which operated between December 2004 and April 2006.
+                     It ceased operations after its Japanese distributor folded."
+                    384)]
+        (is (= "[december, 2004]" (djl/predict predictor input))))))
 
   (testing "Bert QA - Map criteria"
-    (let [input (QAInput.
-                  "When did BBC Japan start broadcasting?"
-                  "BBC Japan was a general entertainment Channel.
-                   Which operated between December 2004 and April 2006.
-                   It ceased operations after its Japanese distributor folded."
-                  384)
-
-          model (djl/load-model {:application Application$NLP/QUESTION_ANSWER
-                                 :input QAInput
-                                 :output String
-                                 :progress (djl/progress-bar)
-                                 :filter {"backbone" "bert"
-                                          "dataset" "book_corpus_wiki_en_uncased"}})]
-      (is (= "[december, 2004]" (-> (djl/predictor model)
-                                    (djl/predict input)))))))
+    (with-open [model (djl/load-model {:application Application$NLP/QUESTION_ANSWER
+                                       :input QAInput
+                                       :output String
+                                       :progress (djl/progress-bar)
+                                       :filter {"backbone" "bert"
+                                                "dataset" "book_corpus_wiki_en_uncased"}})
+                predictor (djl/predictor model)]
+      (let [input (QAInput.
+                    "When did BBC Japan start broadcasting?"
+                    "BBC Japan was a general entertainment Channel.
+                     Which operated between December 2004 and April 2006.
+                     It ceased operations after its Japanese distributor folded."
+                    384)]
+        (is (= "[december, 2004]" (djl/predict predictor input)))))))
